c3e2285d79c0e439c45479e5b2202bf3b0476799
Your Name <you@example.com>
2024-02-15 17:47:55 -0500

    defined restaurant lists

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,3 +1,13 @@
-export default function RestaurantList() {
-    return <div>RestaurantList</div>;
-    }
+import {useEffect} from 'react';
+export default function RestaurantList({loadRestaurants, restaurants}) {
+    useEffect(() => {
+    loadRestaurants();
+    }, [loadRestaurants]);
+    return (
+    <ul>
+    {restaurants.map(restaurant => (
+    <li key={restaurant.id}>{restaurant.name}</li>
+    ))}
+    </ul>
+    );
+    };
