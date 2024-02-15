5f1aab2612d8c95dee76775a1e4867e8481c27ec
Your Name <you@example.com>
2024-02-15 09:58:58 -0500

    save

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+export default function RestaurantList({loadRestaurants, restaurants}) {
+    useEffect(() => {
+    loadRestaurants();
+    }, [loadRestaurants]);
+
+    return (
+        <ul>
+        {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+        ))}
+        </ul>
+    );
+};
