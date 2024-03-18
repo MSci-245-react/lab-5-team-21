975708190c2333b93071c39beda06c1f64313d00
Nicole <nicole7443thapa@gmail.com>
2024-03-18 10:59:41 -0400

    did lab 10 upto cypress part

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,17 +1,20 @@
 import {useEffect} from 'react';
+import {connect} from 'react-redux';
 
+export function RestaurantList({loadRestaurants, restaurants}) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+  return (
+    <ul>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
+}
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
 
-export default function RestaurantList({loadRestaurants, restaurants}) {
-    useEffect(() => {
-        loadRestaurants();
-    }, [loadRestaurants]);
- 
-    return (
-        <ul>
-            {restaurants.map(restaurant => (
-                <li key={restaurant.id}>{restaurant.name}</li>
-            ))}
-        </ul>
-    );
-};
-
+export default connect(mapStateToProps)(RestaurantList);
